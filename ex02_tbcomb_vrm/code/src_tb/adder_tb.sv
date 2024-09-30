98dc0e721a4a17dbe50dfa540e01d2cf35cce4f5
André <andre_miguel_costa@hotmail.com>
2024-09-30 18:13:26 +0200

    feat: add generic variables

--- ex02_tbcomb_vrm/code/src_tb/adder_tb.sv
+++ ex02_tbcomb_vrm/code/src_tb/adder_tb.sv
@@ -27,102 +27,120 @@ Ver   Date        Person     Comments
 
 *******************************************************************************/
 
-interface adder_in_itf;
-    logic[7:0] a;
-    logic[7:0] b;
-    logic      carry;
+interface adder_in_if #(
+    int DATASIZE = 8
+);
+  logic [DATASIZE-1:0] a;
+  logic [DATASIZE-1:0] b;
+  logic                carry;
 endinterface
 
-interface adder_out_itf;
-    logic[7:0] result;
-    logic      carry;
+interface adder_out_if #(
+    int DATASIZE = 8
+);
+
+  logic [DATASIZE-1:0] result;
+  logic                carry;
 endinterface
 
-module adder_tb;
+module adder_tb #(
+    int DATASIZE = 8,
+    int TESTCASE = 0
+);
 
-    timeunit 1ns;         // Definition of the time unit
-    timeprecision 1ns;    // Definition of the time precision
-   
-    // Reference
-    logic[7:0] result_ref;
-   
-    // Timings definitions
-    time sim_step = 10ns;
-    time pulse = 0ns;
-   
-    logic error_signal = 0;
-   
-    logic synchro = 0;
-   
-    always #(sim_step/2) synchro = ~synchro;
-   
-    // Integer to keep track of the number of errors
-    int nb_errors = 0;
-   
-    adder_in_itf input_itf();
-    adder_out_itf output_itf();
-   
-    // DUV instantiation
-    adder duv(.a_i(input_itf.a),
-              .b_i(input_itf.b),
-              .carryin_i(input_itf.carry),
-              .result_o(output_itf.result),
-              .carryout_o(output_itf.carry));
+  timeunit 1ns;  // Definition of the time unit
+  timeprecision 1ns;  // Definition of the time precision
+
+  // Reference
+  logic [DATASIZE-1:0] result_ref;
+
+  // Timings definitions
+  time sim_step = 10ns;
+  time pulse = 0ns;
+
+  logic error_signal = 0;
+
+  logic synchro = 0;
+
+  always #(sim_step / 2) synchro = ~synchro;
+
+  // Integer to keep track of the number of errors
+  int nb_errors = 0;
+
+  adder_in_if #(DATASIZE) input_itf ();
+  adder_out_if #(DATASIZE) output_itf ();
+
+  // DUV instantiation
+  adder duv (
+      .a_i(input_itf.a),
+      .b_i(input_itf.b),
+      .carryin_i(input_itf.carry),
+      .result_o(output_itf.result),
+      .carryout_o(output_itf.carry)
+  );
 
 
-    task test_scenario0;
-        for(int a = 0; a < 256; a++) begin
-            for(int b = 0; b < 256; b++) begin
-                input_itf.a = a;
-                input_itf.b = b;
-                compute_reference(input_itf.a, input_itf.b, result_ref);
-                @(posedge(synchro));
-            end
-        end
-    endtask
-
-    task compute_reference(logic[7:0] a, logic[7:0] b, output logic[8:0] result);
-        result = a + b;
-    endtask
-
-    task compute_reference_task;
-        forever begin
-            @(posedge(synchro));
-            #1;
-            compute_reference(input_itf.a, input_itf.b, result_ref);
-        end
-    endtask
-
-    task verification;
-        @(negedge(synchro));
-        forever begin
-            if (output_itf.result != result_ref) begin
-                nb_errors ++;
-                $display("Error for a_i = %d and b_i = %d. Expected: %d, Observed: %d", input_itf.a, input_itf.b, result_ref, output_itf.result);
-                error_signal = 1;
-                #pulse;
-                error_signal = 0;
-            end
-            @(negedge(synchro));
-        end
-    endtask
-
-    initial begin
-
-        $display("Starting simulation");
-        fork
-            test_scenario0;
-            compute_reference_task;
-            verification;
-        join_any
-
-        $display("Ending simulation");
-        if (nb_errors > 0)
-            $display("Number of errors : %d", nb_errors);
-        else
-            $display("No errors");
-
-        $finish(0);
+  task automatic test_scenario0;
+    for (int a = 0; a < 256; a++) begin
+      for (int b = 0; b < 256; b++) begin
+        input_itf.a = a;
+        input_itf.b = b;
+        compute_reference(input_itf.a, input_itf.b, result_ref);
+        @(posedge (synchro));
+      end
     end
+  endtask
+
+  task automatic compute_reference(logic [DATASIZE-1:0] a, logic [DATASIZE-1:0] b,
+                                   output logic [DATASIZE : 0] result);
+    result = a + b;
+  endtask
+
+  task automatic compute_reference_task;
+    forever begin
+      @(posedge (synchro));
+      #1;
+      compute_reference(input_itf.a, input_itf.b, result_ref);
+    end
+  endtask
+
+  task automatic verification;
+    @(negedge (synchro));
+    forever begin
+      if (output_itf.result != result_ref) begin
+        nb_errors++;
+        $display("Error for a_i = %d and b_i = %d. Expected: %d, Observed: %d", input_itf.a,
+                 input_itf.b, result_ref, output_itf.result);
+        error_signal = 1;
+        #pulse;
+        error_signal = 0;
+      end
+      @(negedge (synchro));
+    end
+  endtask
+
+  task automatic test_scenario;
+    case (TESTCASE)
+      0: test_scenario0;
+      default: $diplay("Invalid test case %d", TESTCASE);
+    endcase
+
+  endtask
+
+  initial begin
+
+    $display("Starting simulation");
+    fork
+      test_scenario;
+      compute_reference_task;
+      verification;
+    join_any
+
+    $display("Ending simulation");
+    if (nb_errors > 0) $display("Number of errors : %d", nb_errors);
+    else $display("No errors");
+
+    $finish(0);
+  end
 
 endmodule
