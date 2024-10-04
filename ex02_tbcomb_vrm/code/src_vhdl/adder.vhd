61824797582df3c2c501cc4eea33941661721964
André <andre_miguel_costa@hotmail.com>
2024-10-04 16:03:43 +0200

    feat: back to size

--- ex02_tbcomb_vrm/code/src_vhdl/adder.vhd
+++ ex02_tbcomb_vrm/code/src_vhdl/adder.vhd
@@ -16,38 +16,38 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity adder is
-generic( DATASIZE: integer := 8);
+generic( SIZE: integer := 8);
 port(
 	carryin_i: in std_logic;
 	carryout_o: out std_logic;
-	a_i: in std_logic_vector(DATASIZE-1 downto 0);
-	b_i: in std_logic_vector(DATASIZE-1 downto 0);
-	result_o: out std_logic_vector(DATASIZE-1 downto 0)
+	a_i: in std_logic_vector(SIZE-1 downto 0);
+	b_i: in std_logic_vector(SIZE-1 downto 0);
+	result_o: out std_logic_vector(SIZE-1 downto 0)
 );
 end adder;
 
 architecture behave of adder is
 
-	signal a_s      : unsigned(DATASIZE downto 0);
-	signal b_s      : unsigned(DATASIZE downto 0);
-	signal result_s : unsigned(DATASIZE downto 0);
-	signal c_s      : unsigned(DATASIZE downto 0);
+	signal a_s      : unsigned(SIZE downto 0);
+	signal b_s      : unsigned(SIZE downto 0);
+	signal result_s : unsigned(SIZE downto 0);
+	signal c_s      : unsigned(SIZE downto 0);
 begin
 
-	a_s(DATASIZE)            <= '0';
-	a_s(DATASIZE-1 downto 0) <= unsigned(a_i);
+	a_s(SIZE)            <= '0';
+	a_s(SIZE-1 downto 0) <= unsigned(a_i);
 	
-	b_s(DATASIZE)            <= '0';
-	b_s(DATASIZE-1 downto 0) <= unsigned(b_i);
+	b_s(SIZE)            <= '0';
+	b_s(SIZE-1 downto 0) <= unsigned(b_i);
 	
 	c_s(0)               <= carryin_i;
-	c_s(DATASIZE downto 1)   <= (others=>'0');
+	c_s(SIZE downto 1)   <= (others=>'0');
 	
 	result_s             <= a_s + b_s + c_s;
 	
-	carryout_o           <= result_s(DATASIZE);
+	carryout_o           <= result_s(SIZE);
 
-	result_o             <= std_logic_vector(result_s(DATASIZE-1 downto 0));
+	result_o             <= std_logic_vector(result_s(SIZE-1 downto 0));
 
 end behave;
 
