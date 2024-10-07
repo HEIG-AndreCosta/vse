16949c3351278e570f06ee6a373869cf6357990c
André <andre_miguel_costa@hotmail.com>
2024-10-07 18:07:54 +0200

    feat: rtest and stest constraints

--- ex03_sv_random/code/src_tb/test_random_tb.sv
+++ ex03_sv_random/code/src_tb/test_random_tb.sv
@@ -34,27 +34,94 @@ module test_random_tb;
 
 
   class RTest;
+    rand bit [15:0] a;
+    rand bit [15:0] b;
+    rand bit [15:0] c;
+    rand bit [ 1:0] m;
+    constraint m_c {m inside {[0 : 2]};}
+    constraint a_c {
+      (m == 0) -> a < 10;
+      (m == 1) -> b inside {[12 : 16]};
+    }
+
+    constraint c_c {c > (a + b);}
+    constraint order_a_c {solve m before a, b;}
+    constraint order_c_c {solve a, b before c;}
+
+  endclass
+
+  class STest;
+    rand bit [7:0] sa;
+    rand bit [7:0] sb;
+
+    constraint pair_c {sa[0] == 1'b0 -> sb[0] == 1'b0;}
+    constraint order_c {solve sa before sb;}
 
   endclass
 
 
-  task test_case0();
-
-    a = 0;
-    b = 0;
-    c = 0;
-    m = 0;
+  task automatic test_case0();
+    RTest obj;
+    a   = 0;
+    b   = 0;
+    c   = 0;
+    m   = 0;
 
     // Create the object
-
+    obj = new();
 
     ##1;
 
-    repeat (10) begin
+    repeat (1000) begin
       // Randomize the object
+      assert (obj.randomize())
+      else $fatal("No solutions for obj.randomize");
+
+      assert (m >= 0 && m <= 2)
+      else $fatal("M constraint failed. Expected  0 <= %d <= 2", obj.m);
+
+      if (obj.m == 0) begin
+        assert (obj.a < 10)
+        else $fatal("A constraint failed. m == 0. Expected a to be < 10. Actual value: %d", obj.a);
+      end else if (obj.m == 1) begin
+        assert (obj.b >= 12 && obj.b <= 16)
+        else
+          $fatal(
+              "B constraint failed. m == 1. Expected b to be >= 12 && obj.b <= 16."+
+              "Actual value: %d",
+              obj.b
+          );
+      end
+      assert (obj.c > obj.a + obj.b)
+      else $fatal("C constraint failed. Expected %d > %d + %d.", obj.c, obj.a, obj.b);
 
       // Apply its values to the signals (for nice view in the chronogram)
+      a = obj.a;
+      b = obj.b;
+      c = obj.c;
+      m = obj.m;
       ##1;
+    end
+  endtask
+
+  task automatic test_case1();
+
+    STest obj;
+    // Create the object
+    obj = new();
+
+    ##1;
+    repeat (1000) begin
+
+      assert (obj.randomize())
+      else $fatal("No solutions for obj.randomize");
+
+      if (obj.sa[0] == 1'b0) begin
+        assert (obj.sb[0] == 1'b0)
+        else $fatal("Expected sb to be pair because sa is pair (%d)", obj.sb);
+      end
+
+
     end
   endtask
 
@@ -64,6 +131,7 @@ module test_random_tb;
   program TestSuite;
     initial begin
       test_case0();
+      test_case1();
       $stop;
     end
 
