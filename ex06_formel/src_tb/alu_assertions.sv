e6ce1181a0a66bc05684bdb5a9a5766959d6d4aa
Yann Thoma <yann.thoma@heig-vd.ch>
2024-11-08 17:41:35 +0100

    All assertions modules only have input ports

--- ex06_formel/src_tb/alu_assertions.sv
+++ ex06_formel/src_tb/alu_assertions.sv
@@ -2,7 +2,7 @@ module alu_assertions(
     input  logic[2:0] Op,
     input  logic[7:0] A,
     input  logic[7:0] B,
-    output logic[7:0] Y,
-    output logic      F
+    input  logic[7:0] Y,
+    input  logic      F
 );
 endmodule
