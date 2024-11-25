fa7224c6498f564f4e5a4e2bdbd07d32e8526692
André <andre_miguel_costa@hotmail.com>
2024-11-25 18:08:21 +0100

    feat: add avalon driver

--- labo2_uart/code/src_tb/avalon_agent/avalon_driver.sv
+++ labo2_uart/code/src_tb/avalon_agent/avalon_driver.sv
@@ -46,45 +46,97 @@ class avalon_driver #(
 
   virtual avalon_itf vif;
 
+  task wait_ready();
+    while (vif.waitrequest_o) begin
+      @(posedge vif.clk_i);
+    end
+
+  endtask
+  task do_read(logic [13:0] address);
+    wait_ready;
+    vif.address_i = address;
+    vif.write_i = 0;
+    vif.read_i = 1;
+    while (!vif.readdatavalid_o) begin
+      @(posedge vif.clk_i);
+      vif.read_i = 0;
+    end
+    vif.write_i = 0;
+  endtask
+  task do_write(logic [13:0] address, logic [31:0] data);
+    wait_ready;
+    vif.address_i = address;
+    vif.write_i = 1;
+    vif.read_i = 0;
+    @(posedge vif.clk_i);
+    while (!vif.readdatavalid_o) begin
+      @(posedge vif.clk_i);
+      vif.read_i = 0;
+    end
+    vif.write_i = 0;
+  endtask
+
+  task read_status_register();
+    do_read(0);
+  endtask
+
+  task send_tx_data(logic [31:0] data);
+    do_write(1, data);
+  endtask
+
+  task read_rx_data();
+    do_read(2);
+  endtask
+
+  task set_clk_per_bit(logic [31:0] clk_per_bit);
+    do_write(3, clk_per_bit);
+  endtask
+
   task do_transaction(avalon_transaction transaction);
+    automatic logic [31:0] status;
+    $display("[AVL Driver] Do Transaction %d", transaction.transaction_type);
     case (transaction.transaction_type)
       UART_SEND: begin
-        vif.address_i = 1;
-        vif.write_i = 1;
-        vif.read_i = 0;
-        vif.writedata_i = transaction.data;
-        @(negedge vif.clk_i);
-        vif.write_i = 0;
+        send_tx_data(transaction.data);
         avalon_to_scoreboard_tx_fifo.put(transaction);
       end
       UART_READ: begin
-        vif.address_i = 2;
-        vif.write_i = 0;
-        vif.read_i = 1;
-        while (!vif.readdatavalid_o) begin
-          @(negedge vif.clk_i);
-          vif.read_i = 0;
-        end
-        vif.read_i = transaction.data;
+        read_rx_data();
+        transaction.data = vif.readdata_o;
         avalon_to_scoreboard_rx_fifo.put(transaction);
       end
-      WRITE_REGISTER: begin
-        vif.address_i = 3;
-        vif.write_i = 1;
-        vif.read_i = 0;
-        vif.writedata_i = transaction.data;
-        @(negedge vif.clk_i);
-        vif.write_i = 0;
+      ASSERT_TX_FIFO_EMPTY: begin
+        read_status_register;
+        status = vif.readdata_o;
+        assert (!(status & 32'h1));
+        assert (status & 32'h8);
       end
-      STATUS_READ: begin
-        vif.address_i = 0;
-        vif.write_i = 0;
-        vif.read_i = 1;
-        while (!vif.readdatavalid_o) begin
-          @(negedge vif.clk_i);
-          vif.read_i = 0;
-        end
-        vif.read_i = transaction.data;
+      ASSERT_RX_FIFO_FULL: begin
+        read_status_register;
+        status = vif.readdata_o;
+        assert (status & 32'h2);
+        assert (status & 32'h4);
+      end
+      ASSERT_RX_FIFO_HAS_DATA: begin
+        read_status_register;
+        status = vif.readdata_o;
+        assert (!(status & 32'h2));
+        assert (status & 32'h4);
+      end
+      ASSERT_RX_FIFO_EMPTY: begin
+        read_status_register;
+        status = vif.readdata_o;
+        assert (!(status & 32'h2));
+        assert (!(status & 32'h4));
+      end
+      ASSERT_TX_FIFO_FULL: begin
+        read_status_register;
+        status = vif.readdata_o;
+        assert (status & 32'h1);
+        assert (!(status & 32'h8));
+      end
+      SET_CLK_PER_BIT: begin
+        set_clk_per_bit;
       end
       default: begin
       end  //should never get here
@@ -108,8 +160,8 @@ class avalon_driver #(
     @(posedge vif.clk_i);
     @(posedge vif.clk_i);
     while (1) begin
+      @(posedge vif.clk_i);
       sequencer_to_driver_fifo.get(transaction);
-      @(negedge vif.clk_i);
       do_transaction(transaction);
     end
 
