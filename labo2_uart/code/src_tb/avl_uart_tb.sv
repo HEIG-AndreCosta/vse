0dfa2baa1a94bb10bd792641b17b12f1be1d667b
André <andre_miguel_costa@hotmail.com>
2024-11-29 16:18:30 +0100

    refactor: formatting

--- labo2_uart/code/src_tb/avl_uart_tb.sv
+++ labo2_uart/code/src_tb/avl_uart_tb.sv
@@ -50,76 +50,81 @@ Ver   Date        Person     Comments
 
 `include "avl_uart_interface_assertions.sv"
 
-module avl_uart_tb#(int TESTCASE = 0, int DATASIZE = 20, int FIFOSIZE = 10, int ERRNO = 0);
+module avl_uart_tb #(
+    int TESTCASE = 0,
+    int DATASIZE = 20,
+    int FIFOSIZE = 10,
+    int ERRNO = 0
+);
 
-    import objections_pkg::*;
+  import objections_pkg::*;
 
-    avalon_itf local_itf();
-    uart_itf remote_itf();
+  avalon_itf local_itf ();
+  uart_itf remote_itf ();
 
-    avl_uart_env#(DATASIZE, FIFOSIZE) env;
+  avl_uart_env #(DATASIZE, FIFOSIZE) env;
 
-    logic clk_i;
+  logic clk_i;
 
-    default clocking cb @(posedge clk_i);
-    endclocking
+  default clocking cb @(posedge clk_i);
+  endclocking
 
-    assign clk_i = local_itf.clk_i;
+  assign clk_i = local_itf.clk_i;
 
-    avl_uart_interface#(DATASIZE, FIFOSIZE, ERRNO) duv(
-        // Avalon Bus
-        .avl_clk_i(local_itf.clk_i),
-        .avl_reset_i(local_itf.rst_i),
-        .avl_address_i(local_itf.address_i),
-        .avl_byteenable_i(local_itf.byteenable_i),
-        .avl_write_i(local_itf.write_i),
-        .avl_writedata_i(local_itf.writedata_i),
-        .avl_read_i(local_itf.read_i),
-        .avl_readdatavalid_o(local_itf.readdatavalid_o),
-        .avl_readdata_o(local_itf.readdata_o),
-        .avl_waitrequest_o(local_itf.waitrequest_o),
+  avl_uart_interface #(DATASIZE, FIFOSIZE, ERRNO) duv (
+      // Avalon Bus
+      .avl_clk_i(local_itf.clk_i),
+      .avl_reset_i(local_itf.rst_i),
+      .avl_address_i(local_itf.address_i),
+      .avl_byteenable_i(local_itf.byteenable_i),
+      .avl_write_i(local_itf.write_i),
+      .avl_writedata_i(local_itf.writedata_i),
+      .avl_read_i(local_itf.read_i),
+      .avl_readdatavalid_o(local_itf.readdatavalid_o),
+      .avl_readdata_o(local_itf.readdata_o),
+      .avl_waitrequest_o(local_itf.waitrequest_o),
 
-        // UART Bus
-        .rx_i(remote_itf.rx_i),
-        .tx_o(remote_itf.tx_o)
-    );
+      // UART Bus
+      .rx_i(remote_itf.rx_i),
+      .tx_o(remote_itf.tx_o)
+  );
 
-    // Binding of the DUV and the assertions module
-    bind duv avl_uart_interface_assertions #(DATASIZE, FIFOSIZE) binded(.*);
+  // Binding of the DUV and the assertions module
+  bind duv avl_uart_interface_assertions #(DATASIZE, FIFOSIZE) binded (.*);
 
-    // clock generation
-    always #10 local_itf.clk_i = ~local_itf.clk_i;
+  // clock generation
+  always #10 local_itf.clk_i = ~local_itf.clk_i;
 
-    initial begin
-        // Format to print time (%t) in nanosecond, with " ns" suffix.
-        // This allows to click on log in QuestaSim to go to the see it in the wave
-        $timeformat(-9, 0, " ns", 15);
+  initial begin
+    // Format to print time (%t) in nanosecond, with " ns" suffix.
+    // This allows to click on log in QuestaSim to go to the see it in the wave
+    $timeformat(-9, 0, " ns", 15);
 
-        // Set the drain time for objections, 50000 for aprox two times the baudrate
-        objections_pkg::objection::get_inst().set_drain_time(10000 * 5);
+    // Set the drain time for objections, 50000 for aprox two times the baudrate
+    objections_pkg::objection::get_inst().set_drain_time(10000 * 5);
 
-        // Wait for the TB to start
-        ##10;
-        while (!objections_pkg::objection::get_inst().should_finish()) begin
-            ##1;
-        end
-
-        env.end_display();
-
-        $stop;
+    // Wait for the TB to start
+    ##10;
+    while (!objections_pkg::objection::get_inst().should_finish()) begin
+      ##1;
     end
 
-    initial begin
-        // Building the entire environment
-        env = new;
-        env.local_itf = local_itf;
-        env.remote_itf = remote_itf;
-        env.testcase = TESTCASE;
-        env.build();
-        env.connect();
-        env.run();
+    env.end_display();
 
-        $finish;
-    end
+    $stop;
+  end
+
+  initial begin
+    // Building the entire environment
+    env = new;
+    env.local_itf = local_itf;
+    env.remote_itf = remote_itf;
+    env.testcase = TESTCASE;
+    env.build();
+    env.connect();
+    env.run();
+
+    $finish;
+  end
 
 endmodule : avl_uart_tb
