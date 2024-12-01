6490517eb771aca9444276b69764547e3482582e
André <andre_miguel_costa@hotmail.com>
2024-12-01 12:27:30 +0100

    feat: add avalon assertions

--- labo2_uart/code/src_tb/avl_uart_interface_assertions.sv
+++ labo2_uart/code/src_tb/avl_uart_interface_assertions.sv
@@ -25,11 +25,34 @@ module avl_uart_interface_assertions #(
   assume_readwrite :
   assume property (!(avl_write_i & avl_read_i));
 
+  // En lecture, la donnée est prête un cycle après que avl_read_i est à ’1’.
+  // En lecture, avl_readdatavalid_o s’active lorsque la donnée est disponible.
   assume_readdatavalid_after_read :
   assume property (avl_read_i |=> avl_readdatavalid_o);
 
-  // An example of assume that would trigger an error
-  // assume_nowrite:
-  // assume property (!(avl_write_i));
+  //Le signal byteenable n’est pas utilisé.
+  assume_byteenable_is_not_used :
+  assume property (avl_byteenable_i == 8'hf);
+  //En écriture, avl_waitrequest_o permet de faire patienter le master, selon le fonction-
+  //nement normal du bus avalon.
+
+  assume_wait_request_is_respected :
+  assume property (avl_waitrequest_o && avl_write_i |=> avl_write_i && (avl_writedata_i == $past(
+      avl_writedata_i
+  )));
+
+  // Vérifie que le status du buffer tx est cohérent
+  // i.e le status vide et le status plein ne sont pas les deux à 1
+  assume_status_register_tx_fifo_status_coherence :
+  assume property (avl_read_i && (avl_address_i == 0)
+    |=> (avl_readdata_o[3] == 0 || (avl_readdata_o[3] != avl_readdata_o[0])));
+
+
+  // Vérifie que le status du buffer rx est cohérent
+  // i.e si le buffer est plein, il faut aussi que le registre indique un
+  // élément disponible
+  assume_status_register_rx_fifo_status_coherence :
+  assume property (avl_read_i && (avl_address_i == 0) ##1 avl_readdata_o[1] |-> avl_readdata_o[2]);
+
 
 endmodule
