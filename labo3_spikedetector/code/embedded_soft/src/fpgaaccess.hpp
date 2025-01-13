329b6edbf030941fd6f63ead228b8538875ecfeb
André <andre_miguel_costa@hotmail.com>
2025-01-13 18:04:04 +0100

    feat: create fpga access interface

---
+++ labo3_spikedetector/code/embedded_soft/src/fpgaaccess.hpp
@@ -0,0 +1,18 @@
+#include <cstdint>
+#include <string>
+
+/**
+ * Type for the IRQ handler function.
+ * That handler should NOT block as it will by directly called
+ * by the receiving thread. And so, if blocked, no new message
+ * will be received, which can lead to deadlocks.
+ */
+typedef void (*irq_handler_t)(const std::string &);
+
+class FpgaAccess {
+    public:
+	virtual void setup() = 0;
+	virtual void write_register(uint16_t reg, uint16_t value) = 0;
+	virtual uint16_t read_register(uint16_t reg) = 0;
+	virtual void set_callback(irq_handler_t) = 0;
+};
