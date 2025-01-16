f2ff944c197852aee70242362f1c55d5f2e6834a
André <andre_miguel_costa@hotmail.com>
2025-01-16 22:34:50 +0100

    feat(mock_fpga_access): add mock registers

--- labo3_spikedetector/code/embedded_soft/test/mock/mock_fpga_access.cpp
+++ labo3_spikedetector/code/embedded_soft/test/mock/mock_fpga_access.cpp
@@ -1,8 +1,10 @@
-
+#include <cassert>
+#include <gtest/gtest.h>
 #include "mock_fpga_access.hpp"
-MockFpgaAccess::MockFpgaAccess()
+MockFpgaAccess::MockFpgaAccess(const std::vector<Register> &registers)
 	: setup_called(false)
 	, access()
+	, registers(registers)
 {
 }
 void MockFpgaAccess::setup()
@@ -20,12 +22,18 @@ void MockFpgaAccess::write_register(uint16_t reg, uint16_t value)
 
 uint16_t MockFpgaAccess::read_register(uint16_t reg)
 {
-	access.push_back(Access{
-		.is_read = true,
-		.reg = reg,
-		.value = 0,
-	});
-	return 0;
+	for (const auto &r : registers) {
+		if (r.address == reg) {
+			access.push_back(Access{
+				.is_read = true,
+				.reg = reg,
+				.value = r.value,
+			});
+
+			return r.value;
+		}
+	}
+	return 0xFFFF;
 }
 void MockFpgaAccess::set_callback(irq_handler_t cb)
 {
--- labo3_spikedetector/code/embedded_soft/test/mock/mock_fpga_access.hpp
+++ labo3_spikedetector/code/embedded_soft/test/mock/mock_fpga_access.hpp
@@ -10,18 +10,30 @@ struct Access {
 	int reg;
 	int value;
 };
+struct Register {
+	uint16_t address;
+	uint16_t value;
+
+	Register(uint16_t address, uint16_t value)
+		: address(address)
+		, value(value)
+	{
+	}
+};
 
 class MockFpgaAccess : public FpgaAccess {
     public:
-	MockFpgaAccess();
-	~MockFpgaAccess();
+	MockFpgaAccess(const std::vector<Register> &registers);
+	~MockFpgaAccess() = default;
 
 	void setup();
 	void write_register(uint16_t reg, uint16_t value);
 
 	uint16_t read_register(uint16_t reg);
 	void set_callback(irq_handler_t);
+
 	std::vector<Access> access;
+	const std::vector<Register> &registers;
 	bool setup_called;
 	irq_handler_t handler;
 
