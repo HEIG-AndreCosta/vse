ca8e183a5c8ee4cbaf84b9fbcc82b43f4841915b
André <andre_miguel_costa@hotmail.com>
2025-01-18 22:02:43 +0100

    test: add fpga access remote tests

--- labo3_spikedetector/code/embedded_soft/test/unit/test_fpga_access_remote.cpp
+++ labo3_spikedetector/code/embedded_soft/test/unit/test_fpga_access_remote.cpp
@@ -1,9 +1,11 @@
-
+#include <chrono>
+#include <condition_variable>
 #include <fcntl.h>
 #include "fpgaaccessremote.hpp"
 #include <arpa/inet.h>
 #include <cstdint>
 #include <gtest/gtest.h>
+#include <mutex>
 #include <netinet/in.h>
 #include <sys/socket.h>
 
@@ -17,9 +19,7 @@ static int connect_to_server(uint16_t port)
 	assert(sockfd >= 0);
 	assert(inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr) > 0);
 	assert(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) >= 0);
-	int flags = fcntl(sockfd, F_GETFL, 0);
-	fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
-
+	usleep(10000);
 	return sockfd;
 }
 
@@ -33,16 +33,99 @@ TEST(TestFpgaAccessRemote, SetupStartServer)
 	addr.sin_port = htons(opts.port);
 
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	ASSERT_GE(sockfd, 0);
-	ASSERT_GT(inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr), 0);
-	ASSERT_LT(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)), 0);
+	EXPECT_GE(sockfd, 0);
+	EXPECT_GT(inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr), 0);
+	EXPECT_LT(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)), 0);
 
-	auto t = std::thread([&] { access.setup(opts); });
-	t.join();
+	access.setup(opts);
 
-	ASSERT_GE(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)), 0);
+	EXPECT_GE(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)), 0);
 
 	close(sockfd);
+}
+
+TEST(TestFpgaAccessRemote, WriteRegister)
+{
+	SetupOptions opts = { .wait_for_connection = false, .port = 1236 };
+	FpgaAccessRemote access;
+	access.setup(opts);
+	int socket = connect_to_server(opts.port);
+
+	access.write_register(1, 2);
+
+	char msg[50];
+	const char *expected = "wr 1 2\n";
+
+	ssize_t bytes = recv(socket, msg, sizeof(msg), MSG_DONTWAIT);
+	msg[bytes] = 0;
+
+	EXPECT_EQ(bytes, strlen(expected));
+	EXPECT_STREQ(msg, expected);
+	close(socket);
+}
+
+TEST(TestFpgaAccessRemote, ReadRegister)
+{
+	SetupOptions opts = { .wait_for_connection = false, .port = 1237 };
+	FpgaAccessRemote access;
+	access.setup(opts);
+	int socket = connect_to_server(opts.port);
+
+	const char *expected = "rd 1\n";
+	const char *response = "1 10\n";
+	char msg[50];
+
+	//Send the response first else we block
+	write(socket, response, strlen(response) + 1);
+
+	EXPECT_EQ(access.read_register(1), 10);
+
+	ssize_t bytes = recv(socket, msg, sizeof(msg), MSG_DONTWAIT);
+	msg[bytes] = 0;
+
+	EXPECT_EQ(bytes, strlen(expected));
+	EXPECT_STREQ(msg, expected);
+	close(socket);
+}
+
+static std::string received;
+
+static std::condition_variable handler_called;
+static std::mutex handler_called_mutex;
+
+static void handler(const std::string &message)
+{
+	received = message;
+	handler_called.notify_all();
+}
+TEST(TestFpgaAccessRemote, HandlerIsCalledOnIrq)
+{
+	SetupOptions opts = { .wait_for_connection = false, .port = 1238 };
+	FpgaAccessRemote access;
+	access.setup(opts);
+	int socket = connect_to_server(opts.port);
+
+	char msg[50];
+	const char *expected = "irq my fancy message\n";
+
+	//Test that a null callback doesn't crash the app
+	access.set_callback(nullptr);
+
+	write(socket, expected, strlen(expected) + 1);
+	sleep(1);
+
+	access.set_callback(handler);
+
+	std::unique_lock<std::mutex> lk(handler_called_mutex);
+
+	write(socket, expected, strlen(expected) + 1);
+
+	EXPECT_EQ(handler_called.wait_for(lk, std::chrono::milliseconds(500)),
+		  std::cv_status::no_timeout);
+
+	EXPECT_STREQ(received.data(), expected);
+
+	close(socket);
 }
 
 TEST(TestFpgaAccessRemote, EndTestMessageIsSent)
@@ -51,21 +134,17 @@ TEST(TestFpgaAccessRemote, EndTestMessageIsSent)
 	int socket;
 	{
 		FpgaAccessRemote access;
-		auto t = std::thread([&] { access.setup(opts); });
-		t.join();
+		access.setup(opts);
 		socket = connect_to_server(opts.port);
 		shutdown(socket, SHUT_WR);
+		sleep(1);
 	}
 
 	const char *expected_message = "end_test\n";
 	char msg[50];
-
-	size_t read_size;
-	read_size = recv(socket, msg, sizeof(msg), 0);
-
-	ASSERT_GE(read_size, strlen(expected_message));
-	msg[read_size] = 0;
-	ASSERT_STREQ(msg, expected_message);
-
+	ssize_t bytes = recv(socket, msg, sizeof(msg), MSG_DONTWAIT);
+	msg[bytes] = 0;
+	EXPECT_EQ(bytes, strlen(expected_message));
+	EXPECT_STREQ(msg, expected_message);
 	close(socket);
 }
