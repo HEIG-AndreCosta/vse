cc7932037708e8b234cc6c1d1b46b0e6eeba1154
André <andre_miguel_costa@hotmail.com>
2025-01-20 23:55:10 +0100

    fix: generate 16 windows

--- labo3_spikedetector/code/simulation_files/generate.py
+++ labo3_spikedetector/code/simulation_files/generate.py
@@ -8,7 +8,7 @@ content = "0\n" * 900
 with open("zeros.txt", "w") as f:
     f.write(content)
 
-content = ("0\n" * 49 + "1000\n" + "0\n" * 100) * 16
+content = ("0\n" * 49 + "1000\n" + "0\n" * 100) * 17
 
 with open("constant_spikes_16_windows.txt", "w") as f:
     f.write(content)
