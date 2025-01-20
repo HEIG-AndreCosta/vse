2a8b0ba8d34884d80ece7cafaf007cdf33f7d2a9
André <andre_miguel_costa@hotmail.com>
2025-01-20 21:22:55 +0100

    feat: generate linear, zeros and constant spikes input files

--- labo3_spikedetector/code/simulation_files/generate.py
+++ labo3_spikedetector/code/simulation_files/generate.py
@@ -1,7 +1,14 @@
-with open("no_spike.txt", "w") as f:
-    for i in range(10000):
-        f.write(f"{i}\n")
+content = "\n".join([str(i) for i in range(900)])
+
+with open("linear.txt", "w") as f:
+    f.write(content)
+
+content = "0\n" * 900
 
 with open("zeros.txt", "w") as f:
-    for i in range(10000):
-        f.write(f"0\n")
+    f.write(content)
+
+content = ("0\n" * 49 + "1000\n" + "0\n" * 100) * 16
+
+with open("constant_spikes_16_windows.txt", "w") as f:
+    f.write(content)
